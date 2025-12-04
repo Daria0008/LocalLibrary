984016b2ab0f1699be5ad7daa4916d72c5e3ac72
Daria <Daria@Daria.ds>
2025-12-04 17:47:25 +0500

    new template for borrowed books

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
+    path(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
